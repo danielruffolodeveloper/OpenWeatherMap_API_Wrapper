4d02a2fa690533f394dc977d8db0c9e3a09ca85a
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2020-07-17 12:22:04 +1000

    Initial server endpoint setup

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Tiny Weather Endpoint:V1.0.0')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
+})
