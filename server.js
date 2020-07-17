d0434462158af0f14794b48b362cba448b45b540
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2020-07-17 12:33:06 +1000

    Added Dockerfile and .env for hidden config files

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // server.js
 
 const express = require('express')
+require('dotenv').config();
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Tiny Weather Endpoint:V1.0.0')
 })
+
+
 
 app.listen(3000, () => {
     console.log('Server is up on 3000')
